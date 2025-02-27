8eb7759d4a5a3b99edb12a98b008918188d2101a
kanchansingh2004 <kannursingh2004@gmail.com>
2025-02-27 16:58:18 +0530

    [Kanchan] Add. Employee Payroll App with service layer and store employee data

--- src/main/java/com/bridgelabz/employeepayrollapp/employeeservices/EmployeeService.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/employeeservices/EmployeeService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-//UC-01 Services Layer in Employee Payroll App 
+//UC-01 Services Layer in Employee Payroll App
 @Service
 public class EmployeeService {
     private static final Logger logger = LoggerFactory.getLogger(EmployeeService.class);
